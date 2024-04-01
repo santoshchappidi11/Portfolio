93d76d0d6be9bd332acfa679c973677bf4004d72
Santosh_Chappidi <santoshchappidi03@gmail.com>
2024-04-01 22:25:52 +0530

    updates

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -68,7 +68,7 @@ const Home = () => {
               <a href="https://www.linkedin.com/in/santosh-chappidi-a98b59215">
                 <i className="fab fa-linkedin fa-2x"></i>
               </a>
-              <a href="https://drive.google.com/uc?export=download&id=1cTE1AAmVXyGXZ1_8afJO0jqSsMhcuJKp">
+              <a href="https://drive.google.com/file/d/1FtEpOg7Xdj4YOeoaK3XY8KSIckdOXuhE/view?usp=drive_link">
                 {/* <button>Resume</button> */}
                 <motion.button
                   whileTap={{ scale: 0.9 }}
