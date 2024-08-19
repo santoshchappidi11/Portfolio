98e85be0b8bd2caa88e8913249dfa399a4ff275b
Santosh_Chappidi <santoshchappidi03@gmail.com>
2024-08-19 23:49:00 +0530

    updates

--- src/components/Contact-Banner/ContactBanner.js
+++ src/components/Contact-Banner/ContactBanner.js
@@ -12,7 +12,7 @@ const ContactBanner = () => {
       <a href="https://www.linkedin.com/in/santosh-chappidi-a98b59215">
         <i className="fab fa-linkedin fa-2x"></i>
       </a>
-      <a href="https://drive.google.com/uc?export=download&id=1XnarDJD6m47Cmip0yMsp67A3FiBspNOC">
+      <a href="https://drive.google.com/uc?export=download&id=1o2dbCt91gM7P5SYKgIVRZGms_Lt5hA-c">
         <motion.button
           whileTap={{ scale: 0.9 }}
           style={{
